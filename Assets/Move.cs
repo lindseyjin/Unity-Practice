967679754e2a5c1f3ae2aaed3ca193c4e78151f1
Lindsey Jin <l44jin@uwaterloo.ca>
2018-02-20 17:51:44 -0500

    Playing around with Unity

---
+++ Assets/Move.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 direction = Camera.main.transform.forward;
+        float scalar = 10 * Time.deltaTime;
+
+        transform.Translate(direction * scalar); //transform and translate the player
+	}
+}
